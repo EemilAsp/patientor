007919358b5cef63f34f555b87c23a0a65d83cc9
Eemil Asp <Eemil.aspholm@gmail.com>
2023-08-04 12:11:54 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import { Button, Divider, Container, Typography } from '@mui/material';
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom"
+import { Button, Divider, Container, Typography } from "@mui/material"
 
-import { apiBaseUrl } from "./constants";
-import { Patient } from "./types";
+import { apiBaseUrl } from "./constants"
+import { Patient } from "./types"
 
-import patientService from "./services/patients";
-import PatientListPage from "./components/PatientListPage";
+import patientService from "./services/patients"
+import PatientListPage from "./components/PatientListPage"
 
 const App = () => {
-  const [patients, setPatients] = useState<Patient[]>([]);
+  const [patients, setPatients] = useState<Patient[]>([])
 
   useEffect(() => {
-    void axios.get<void>(`${apiBaseUrl}/ping`);
+    void axios.get<void>(`${apiBaseUrl}/ping`)
 
     const fetchPatientList = async () => {
-      const patients = await patientService.getAll();
-      setPatients(patients);
-    };
-    void fetchPatientList();
-  }, []);
-  
+      const patients = await patientService.getAll()
+      setPatients(patients)
+    }
+    void fetchPatientList()
+  }, [])
+
   return (
     <div className="App">
       <Router>
@@ -34,12 +34,20 @@ const App = () => {
           </Button>
           <Divider hidden />
           <Routes>
-            <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
+            <Route
+              path="/"
+              element={
+                <PatientListPage
+                  patients={patients}
+                  setPatients={setPatients}
+                />
+              }
+            />
           </Routes>
         </Container>
       </Router>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
