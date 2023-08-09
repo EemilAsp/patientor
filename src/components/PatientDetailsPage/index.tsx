1d3896bf0ec3b4cc08f2c02e2f9fea7377f22374
Eemil Asp <Eemil.aspholm@gmail.com>
2023-08-09 18:15:00 +0300

    9.23 done

--- src/components/PatientDetailsPage/index.tsx
+++ src/components/PatientDetailsPage/index.tsx
@@ -32,13 +32,38 @@ const PatientDetailsPage = ({ patients }: Props) => {
 
   return (
     <Box>
-      <Typography>
-        <b>
-          {patient.name} {getGender(patient.gender)}
-        </b>
-      </Typography>
-      <div>SSN: {patient.ssn}</div>
-      <div>Occupation: {patient.occupation}</div>
+      <Box>
+        <Box my={2}></Box>
+        <Typography>
+          <b>
+            {patient.name} {getGender(patient.gender)}
+          </b>
+        </Typography>
+        <div>SSN: {patient.ssn}</div>
+        <div>Occupation: {patient.occupation}</div>
+      </Box>
+      <Box>
+        <Box my={2}></Box>
+        <Typography>
+          <b>Entries</b>
+        </Typography>
+        <ul>
+          {patient.entries.map((entry) => (
+            <div>
+              <div>
+                Date: <b>{entry.date}</b>{" "}
+              </div>
+              <div>Description: {entry.description} </div>
+              <div>Diagnosis codes: </div>
+              <ul>
+                {entry.diagnosisCodes.map((code, index) => (
+                  <li key={index}>{code}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </ul>
+      </Box>
     </Box>
   )
 }
